08bdbb12ab444260645104326bb333e444659f41
Luca.Dand <Luca.dandois@student.kdg.be>
2023-02-09 13:27:15 +0100

    vars

--- src/main/java/be/kdg/tic_tac_toe/view/menu/View.java
+++ src/main/java/be/kdg/tic_tac_toe/view/menu/View.java
@@ -5,18 +5,18 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.control.Label;
 
 public class View extends BorderPane{
-    private Label Gamemode;
+    private Label gamemode;
     private Button PvP;
     private Button PvE;
-    private Button Ultra_Nightmare;
-    private Label Bord_size;
+    private Button ultraNightmare;
+    private Label bordSize;
     private Button drie;
     private Button vijf;
     private Button zeven;
     private Button play;
     private Button terug;
     private void initializeNodes() {
-        this.Gamemode = new Label("Gamemode");
+        this.gamemode = new Label("Gamemode");
         this.PvP = new Button("PvP");
         this.PvE = new Button("PvE");
     }
