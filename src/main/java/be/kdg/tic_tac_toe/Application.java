1e9bd41ae2d66478fb41907eb507d82f4f176438
Luca.Dand <Luca.dandois@student.kdg.be>
2023-03-01 15:50:07 +0100

    merge

--- src/main/java/be/kdg/tic_tac_toe/Application.java
+++ src/main/java/be/kdg/tic_tac_toe/Application.java
@@ -2,6 +2,8 @@ package be.kdg.tic_tac_toe;
 import be.kdg.tic_tac_toe.view.game.GamePresenter;
 import be.kdg.tic_tac_toe.view.game.GameView;
 import be.kdg.tic_tac_toe.model.Model;
+import be.kdg.tic_tac_toe.view.home.HomePresenter;
+import be.kdg.tic_tac_toe.view.home.HomeView;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
@@ -10,19 +12,16 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        //TODO temp//
-        GameView view = new GameView(3);
-        //HomeView view = new HomeView();
+
+        HomeView view = new HomeView();
         Scene scene = new Scene(view);
-        //TODO temp//
-        new GamePresenter(view, new Model());
-        //new HomePresenter(view, new Model());
+
+        new HomePresenter(view, new Model());
 
         stage.setScene(scene);
         stage.setTitle("Tic Tac Toe");
-        stage.sizeToScene();
-        //stage.setWidth(900);
-        //stage.setHeight(700);
+        stage.setWidth(900);
+        stage.setHeight(700);
         stage.setResizable(false);
         stage.show();
     }
