5b96cc23e52927acb18e029bd249b94fde1c7ac9
quinten_maas <quinten.maas@student.kdg.be>
2023-03-16 10:31:31 +0100

    commentsssssssssssssssssssssssssssssssssssss

--- src/main/java/be/kdg/tic_tac_toe/model/Contribution.java
+++ src/main/java/be/kdg/tic_tac_toe/model/Contribution.java
@@ -4,7 +4,7 @@ package be.kdg.tic_tac_toe.model;
  * CLass that holds the information of the players and the sorts.
  */
 class Contribution {
-
+    // vars aanmaken, 2 arrays die niet kunnen veranderen
     private final Player[] PLAYERS;
     private final Sort[] SORTS;
 
@@ -14,8 +14,12 @@ class Contribution {
      * @param name1 the name of the first player. (String)
      * @param name2 the name of the second player. (String)
      */
+    // lees dit hiervoor
     Contribution(String name1, String name2) {
+        //contribution voor de gamemode pvp want je geeft 2 namen mee
+        //player geinitializeerd met 2 humans die allebij een naam hebben
         PLAYERS = new Player[]{new Human(name1), new Human(name2)};
+        // er zijn 2 soorten dus daarom de 2
         SORTS = new Sort[2];
     }
 
@@ -25,6 +29,8 @@ class Contribution {
      * @param name the name of the first player. (String)
      */
     Contribution(String name, boolean ai) {
+        //contribution voor pve of ultra nightmare want de ene is een naam en de andere is npc ai
+        //maar in npc staat een functie om te kijken of de makkelijke of de slimme is
         PLAYERS = new Player[]{new Human(name), new NPC(ai)};
         SORTS = new Sort[2];
     }
@@ -36,6 +42,8 @@ class Contribution {
      * @return the sort of the player. (Sort)
      */
     String getSort(int index) {
+        // geeft een string terug met X of O en de index de min 1 staat er om de opties 1 en 2 te kunne gebruiken
+        //met de -1 zet die het terug om naar 0 of 1
         return String.format("%s", SORTS[index - 1]);
     }
 
@@ -46,6 +54,8 @@ class Contribution {
      * @return the name of the player. (String)
      */
     String getName(int index) {
+        // om de naam van de gekozen speler te geven
+        // de -1 weer om 1 en 2 om te zetten naar de plaats in de array van 0 en 1
         return PLAYERS[index - 1].toString();
     }
 
@@ -56,6 +66,7 @@ class Contribution {
      * @return the player. (Player)
      */
     Player getPlayer(int index) {
+        //de speler die gekozen is, -1 om de plaats in de array te bepalen, teruggeven
         return PLAYERS[index - 1];
     }
 
--- src/main/java/be/kdg/tic_tac_toe/model/NPC.java
+++ src/main/java/be/kdg/tic_tac_toe/model/NPC.java
@@ -3,6 +3,7 @@ package be.kdg.tic_tac_toe.model;
 import java.util.Random;
 
 public class NPC extends Player {
+    //de vars initializeren
     private static final int MIN = -1000;
     private static final int MAX = 1000;
     private Random random;
@@ -10,16 +11,21 @@ public class NPC extends Player {
 
 
     NPC(boolean ai) {
+        //geef een naam aan de ai genaamd bot
         super("Bot");
+        // de ai die meegegeven word is de ai van de vars
         this.ai = ai;
+        // als deze ai niet degene is die meegegeven is dan moet je een random gebruiken
         if (!ai){
              this.random = new Random();
         }
     }
 
     void playNPC(Board board, Sort currentSort) {
+        //als de keuze ai is dan moet de minimax aangesproken worden en de beste move uitgevoerd worden
         if (ai){
             this.bestMove(board, currentSort);
+            //word de ai niet aangesproken dan moet de makkelijke computer spelen en dus een gewone random gooien voor de plaats
         }else{
             this.randomMove(board, currentSort);
         }
@@ -27,23 +33,27 @@ public class NPC extends Player {
     }
 
     private void randomMove(Board board, Sort currentSort){
+        // de size van het bord is zo groot als de lengte van de pieces aan mekaar
       int size = board.getPieces().length;
       int x;
       int y;
+      //zolang de pieces niet gelijk zijn aan null dan moet je een random x en y waarde kiezen binnen de size om een piece op te zetten
       do {
            x = random.nextInt(size);
            y = random.nextInt(size);
       }while (board.getPieces()[x][y] != null);
-
+    // de functie place word aangeroepen en de current sort(x of O) word geplaatst op de gekozen y en x coordinaten
       board.place(currentSort, y, x);
 
     }
 
     private boolean movesAble(Board board) {
+        //het bord opnieuw tekenen met al de veranderingen
         return board.draw();
     }
 
     private int evaluation(Board board, Sort ownSort, Sort opponent) {
+        //minimax
         if (board.win(ownSort)) {
             return 10;
         } else if (board.win(opponent)) {
